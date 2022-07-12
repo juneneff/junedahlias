1b56dd02aea39a87484e8c998bd90f92d23da43e
juneneff <84413455+juneneff@users.noreply.github.com>
2022-07-12 09:46:01 -0700

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+	greeting = 'Good evening!';
+} 	else if (hourNow > 12) {
+	greeting = 'Good afternoon!'
+}	else if (hourNow > 0) {
+	greeting = 'Good morning!'
+}	else {
+	greeting = 'Hello!'
+}
+
+document.write('<h2>' + greeting + '</h2>');
