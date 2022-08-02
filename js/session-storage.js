729ac233c0886167f028440fda94801d50ae6070
juneneff <84413455+juneneff@users.noreply.github.com>
2022-08-02 14:49:19 -0700

    Add files via upload

---
+++ js/session-storage.js
@@ -0,0 +1,16 @@
+if (window.sessionStorage) {
+	
+var txtUsername = document.getElementById('username');		//Get form elements
+var txtAnswer = document.getElementById('answer');
+
+txtUsername.value = localStorage.getItem('username');		//Elements populated
+txtAnswer.value = localStorage.getItem('answer');			//by localStorage data
+
+txtUsername.addEventListener('input', function () {			//Data saved
+	sessionStorage.setItem('username', txtUsername.value);
+}, false);
+
+txtAnswer.addEventListener('input', function () {			//Data saved
+	localStorage.setItem('answer', txtAnswer.value);
+}, false);
+}
